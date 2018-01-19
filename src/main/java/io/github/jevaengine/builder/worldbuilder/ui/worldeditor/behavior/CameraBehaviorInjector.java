ac5451db34fe0b07f18b60e7d0389846ff4a4b6a
S <S@DESKTOP-7P77RN7.hitronhub.home>
2018-01-18 18:19:49 -0800

    Added ability to toggle object wireframe.

--- src/main/java/io/github/jevaengine/builder/worldbuilder/ui/worldeditor/behavior/CameraBehaviorInjector.java
+++ src/main/java/io/github/jevaengine/builder/worldbuilder/ui/worldeditor/behavior/CameraBehaviorInjector.java
@@ -26,8 +26,10 @@ import io.github.jevaengine.ui.WindowManager;
 import io.github.jevaengine.ui.WorldView;
 import io.github.jevaengine.world.entity.IEntity;
 import io.github.jevaengine.world.scene.ISceneBuffer;
+import io.github.jevaengine.world.scene.ISceneBuffer.ISceneBufferEffect;
 import io.github.jevaengine.world.scene.ISceneBuffer.ISceneComponentEffect;
 import io.github.jevaengine.world.scene.camera.ControlledCamera;
+import io.github.jevaengine.world.scene.effect.DebugDrawComponent;
 import java.awt.AlphaComposite;
 import java.awt.Composite;
 import java.awt.Graphics2D;
@@ -40,236 +42,253 @@ import java.util.Collection;
  */
 public class CameraBehaviorInjector extends BasicBehaviorInjector {
 
-	private final EditorWorld m_world;
-	private final ControlledCamera m_camera;
+    private final EditorWorld m_world;
+    private final ControlledCamera m_camera;
 
-	public CameraBehaviorInjector(WindowManager windowManager, IWindowFactory windowFactory, EditorWorld world, ControlledCamera camera) {
-		super(windowManager, windowFactory);
-		m_world = world;
-		m_camera = camera;
-	}
+    public CameraBehaviorInjector(WindowManager windowManager, IWindowFactory windowFactory, EditorWorld world, ControlledCamera camera) {
+        super(windowManager, windowFactory);
+        m_world = world;
+        m_camera = camera;
+    }
 
-	@Override
-	protected void doInject() throws NoSuchControlException {
-		final Timer logicTimer = new Timer();
-		final WorldView worldView = getControl(WorldView.class, "worldView");
-		final Label lblCursorCoordinates = getControl(Label.class, "lblCursorCoordinates");
-		final Label lblIsTraversable = getControl(Label.class, "lblIsTraversable");
-		final CameraController cameraController = new CameraController(worldView);
-		
-		m_world.attachCamera(m_camera);
-		
-		addControl(logicTimer);
-		worldView.setCamera(m_camera);
-		
-		m_camera.addEffect(new HideOverCursorEffect());
+    @Override
+    protected void doInject() throws NoSuchControlException {
+        final Timer logicTimer = new Timer();
+        final WorldView worldView = getControl(WorldView.class, "worldView");
+        final Label lblCursorCoordinates = getControl(Label.class, "lblCursorCoordinates");
+        final Label lblIsTraversable = getControl(Label.class, "lblIsTraversable");
+        final CameraController cameraController = new CameraController(worldView);
 
-		logicTimer.getObservers().add(cameraController);
-		worldView.getObservers().add(cameraController);
-		getObservers().add(cameraController);
+        m_world.attachCamera(m_camera);
 
-		logicTimer.getObservers().add(new Timer.ITimerObserver() {
-			@Override
-			public void update(int deltaTime) {
-				Vector3F coordinates = m_world.getCursor().getLocation();
-				lblCursorCoordinates.setText(String.format("%f, %f, %f; Snap: %f", coordinates.x, coordinates.y, coordinates.z, cameraController.getCursorSnapGridSize()));
-				EditorSceneArtifact tile = m_world.getTile(coordinates);
-				lblIsTraversable.setText(tile == null || tile.isTraversable() ? "true" : "false");
-			}
-		});
+        addControl(logicTimer);
+        worldView.setCamera(m_camera);
+        
+        m_camera.addEffect(new HideOverCursorEffect());
 
-		getControl(Button.class, "btnAdjustDepth").getObservers().add(new Button.IButtonPressObserver() {
-			@Override
-			public void onPress() {
-				displayTextInput("Cursor Depth", Float.toString(m_world.getCursor().getLocation().z), new AdjustDepthListener());
-			}
-		});
+        logicTimer.getObservers().add(cameraController);
+        worldView.getObservers().add(cameraController);
+        getObservers().add(cameraController);
 
-		getControl(Button.class, "btnAdjustZoom").getObservers().add(new Button.IButtonPressObserver() {
-			@Override
-			public void onPress() {
-				displayTextInput("Zoom", Float.toString(m_camera.getZoom()), new AdjustZoomListener());
-			}
-		});
+        logicTimer.getObservers().add(new Timer.ITimerObserver() {
+            @Override
+            public void update(int deltaTime) {
+                Vector3F coordinates = m_world.getCursor().getLocation();
+                lblCursorCoordinates.setText(String.format("%f, %f, %f; Snap: %f", coordinates.x, coordinates.y, coordinates.z, cameraController.getCursorSnapGridSize()));
+                EditorSceneArtifact tile = m_world.getTile(coordinates);
+                lblIsTraversable.setText(tile == null || tile.isTraversable() ? "true" : "false");
+            }
+        });
 
-		getControl(Button.class, "btnAdjustSnapGridSize").getObservers().add(new Button.IButtonPressObserver() {
-			@Override
-			public void onPress() {
-				displayTextInput("Snap", Float.toString(cameraController.getCursorSnapGridSize()), new AdjustSnapListener(cameraController));
-			}
-		});
-	}
+        getControl(Button.class, "btnAdjustDepth").getObservers().add(new Button.IButtonPressObserver() {
+            private ISceneBufferEffect effect = new DebugDrawComponent();
+            @Override
+            public void onPress() {
+                displayTextInput("Cursor Depth", Float.toString(m_world.getCursor().getLocation().z), new AdjustDepthListener());
+            }
+        });
 
-	private final class AdjustZoomListener implements ITextInputQueryObserver {
+        getControl(Button.class, "btnToggleWireframe").getObservers().add(new Button.IButtonPressObserver() {
+            private final ISceneBufferEffect effect = new DebugDrawComponent();
+            private boolean applied = false;
+            
+            @Override
+            public void onPress() {
+                if(applied)
+                    m_camera.removeEffect(effect);
+                else
+                    m_camera.addEffect(effect);
+            
+                applied = !applied;
+            }
+        });
 
-		@Override
-		public void okay(String input) {
-			try {
-				m_camera.setZoom(Float.parseFloat(input));
-			} catch (NumberFormatException e) {
-				displayMessage("Depth must be a properly formed floating point.");
-			}
-		}
+        getControl(Button.class, "btnAdjustZoom").getObservers().add(new Button.IButtonPressObserver() {
+            @Override
+            public void onPress() {
+                displayTextInput("Zoom", Float.toString(m_camera.getZoom()), new AdjustZoomListener());
+            }
+        });
 
-		@Override
-		public void cancel() {
-		}
-	}
+        getControl(Button.class, "btnAdjustSnapGridSize").getObservers().add(new Button.IButtonPressObserver() {
+            @Override
+            public void onPress() {
+                displayTextInput("Snap", Float.toString(cameraController.getCursorSnapGridSize()), new AdjustSnapListener(cameraController));
+            }
+        });
+    }
 
-	private final class AdjustSnapListener implements ITextInputQueryObserver {
+    private final class AdjustZoomListener implements ITextInputQueryObserver {
 
-		private final CameraController m_cameraController;
+        @Override
+        public void okay(String input) {
+            try {
+                m_camera.setZoom(Float.parseFloat(input));
+            } catch (NumberFormatException e) {
+                displayMessage("Depth must be a properly formed floating point.");
+            }
+        }
 
-		public AdjustSnapListener(CameraController cameraController) {
-			m_cameraController = cameraController;
-		}
+        @Override
+        public void cancel() {
+        }
+    }
 
-		@Override
-		public void okay(String input) {
-			try {
-				m_cameraController.setCursorSnapGridSize(Float.parseFloat(input));
-			} catch (NumberFormatException e) {
-				displayMessage("Snap grid size must be a properly formed floating point.");
-			}
-		}
+    private final class AdjustSnapListener implements ITextInputQueryObserver {
 
-		@Override
-		public void cancel() {
-		}
-	}
+        private final CameraController m_cameraController;
 
-	private final class AdjustDepthListener implements ITextInputQueryObserver {
+        public AdjustSnapListener(CameraController cameraController) {
+            m_cameraController = cameraController;
+        }
 
-		@Override
-		public void okay(String input) {
-			try {
-				Vector3F cursorLocation = m_world.getCursor().getLocation();
-				cursorLocation.z = Float.parseFloat(input);
-				m_camera.lookAt(cursorLocation);
-			} catch (NumberFormatException e) {
-				displayMessage("Depth must be a properly formed floating point.");
-			}
-		}
+        @Override
+        public void okay(String input) {
+            try {
+                m_cameraController.setCursorSnapGridSize(Float.parseFloat(input));
+            } catch (NumberFormatException e) {
+                displayMessage("Snap grid size must be a properly formed floating point.");
+            }
+        }
 
-		@Override
-		public void cancel() {
-		}
-	}
-	
-	private class HideOverCursorEffect implements ISceneBuffer.ISceneBufferEffect {
+        @Override
+        public void cancel() {
+        }
+    }
 
-		@Override
-		public IRenderable getUnderlay(Rect2D bounds, Matrix3X3 projection) {
-			return new NullGraphic();
-		}
-		
+    private final class AdjustDepthListener implements ITextInputQueryObserver {
 
-		@Override
-		public IRenderable getOverlay(Rect2D bounds, Matrix3X3 projection) {
-			return new NullGraphic();
-		}
+        @Override
+        public void okay(String input) {
+            try {
+                Vector3F cursorLocation = m_world.getCursor().getLocation();
+                cursorLocation.z = Float.parseFloat(input);
+                m_camera.lookAt(cursorLocation);
+            } catch (NumberFormatException e) {
+                displayMessage("Depth must be a properly formed floating point.");
+            }
+        }
 
-		@Override
-		public ISceneBuffer.ISceneComponentEffect[] getComponentEffect(final Graphics2D g, int offsetX, int offsetY, float scale, Vector2D renderLocation, Matrix3X3 projection, ISceneBuffer.ISceneBufferEntry subject, Collection<ISceneBuffer.ISceneBufferEntry> beneath) {
-			IEntity dispatcher = subject.getDispatcher();
-			
-			if(dispatcher == null || dispatcher.getBody().getLocation().z < m_world.getCursor().getLocation().z + 0.0001)
-				return new ISceneComponentEffect[0];
-			
-			return new ISceneComponentEffect[] {
-				new ISceneComponentEffect() {
-				private Composite m_oldComposite;
-				@Override
-				public void prerender() {
-					m_oldComposite = g.getComposite();
-					g.setComposite(AlphaComposite.SrcOver.derive(0.1f));
-				}
+        @Override
+        public void cancel() {
+        }
+    }
 
-				@Override
-				public void postrender() {
-					g.setComposite(m_oldComposite);
-				}
-			}
-			};
-		}
-		
-	}
+    private class HideOverCursorEffect implements ISceneBuffer.ISceneBufferEffect {
 
-	private class CameraController implements Window.IWindowFocusObserver, Timer.ITimerObserver, WorldView.IWorldViewInputObserver {
+        @Override
+        public IRenderable getUnderlay(Rect2D bounds, Matrix3X3 projection) {
+            return new NullGraphic();
+        }
 
-		private Vector3F m_cameraMovement = new Vector3F();
-		private float m_snapGridSize = 0.5F;
-		private final WorldView m_worldView;
+        @Override
+        public IRenderable getOverlay(Rect2D bounds, Matrix3X3 projection) {
+            return new NullGraphic();
+        }
 
-		public CameraController(WorldView worldView) {
-			m_worldView = worldView;
-		}
+        @Override
+        public ISceneBuffer.ISceneComponentEffect[] getComponentEffect(final Graphics2D g, int offsetX, int offsetY, float scale, Vector2D renderLocation, Matrix3X3 projection, ISceneBuffer.ISceneBufferEntry subject, Collection<ISceneBuffer.ISceneBufferEntry> beneath) {
+            IEntity dispatcher = subject.getDispatcher();
 
-		@Override
-		public void update(int deltaTime) {
-			if (!m_cameraMovement.isZero()) {
-				m_camera.move(m_cameraMovement.normalize().multiply(deltaTime / 200.0F * m_snapGridSize));
-			}
-			float x = (int) (m_camera.getLookAt().x / m_snapGridSize) * m_snapGridSize;
-			float y = Math.round(m_camera.getLookAt().y / m_snapGridSize) * m_snapGridSize;
-			m_world.getCursor().setLocation(new Vector3F(x, y, m_camera.getLookAt().z));
-		}
+            if (dispatcher == null || dispatcher.getBody().getLocation().z < m_world.getCursor().getLocation().z + 0.0001) {
+                return new ISceneComponentEffect[0];
+            }
 
-		public float getCursorSnapGridSize() {
-			return m_snapGridSize;
-		}
+            return new ISceneComponentEffect[]{
+                new ISceneComponentEffect() {
+                    private Composite m_oldComposite;
 
-		public void setCursorSnapGridSize(float gridSize) {
-			m_snapGridSize = Math.max(gridSize, 0.01F);
-		}
+                    @Override
+                    public void prerender() {
+                        m_oldComposite = g.getComposite();
+                        g.setComposite(AlphaComposite.SrcOver.derive(0.1f));
+                    }
 
-		@Override
-		public void onFocusChanged(boolean hasFocus) {
-			if (!hasFocus) {
-				m_cameraMovement = new Vector3F();
-			}
-		}
+                    @Override
+                    public void postrender() {
+                        g.setComposite(m_oldComposite);
+                    }
+                }
+            };
+        }
 
-		@Override
-		public void keyEvent(InputKeyEvent e) {
-			if (e.type == InputKeyEvent.KeyEventType.KeyDown) {
-				switch (e.keyCode) {
-					case KeyEvent.VK_UP:
-						m_cameraMovement.y = -1;
-						break;
-					case KeyEvent.VK_RIGHT:
-						m_cameraMovement.x = 1;
-						break;
-					case KeyEvent.VK_DOWN:
-						m_cameraMovement.y = 1;
-						break;
-					case KeyEvent.VK_LEFT:
-						m_cameraMovement.x = -1;
-						break;
-				}
-			} else if (e.type == InputKeyEvent.KeyEventType.KeyUp) {
-				switch (e.keyCode) {
-					case KeyEvent.VK_UP:
-					case KeyEvent.VK_DOWN:
-						m_cameraMovement.y = 0;
-						break;
-					case KeyEvent.VK_RIGHT:
-					case KeyEvent.VK_LEFT:
-						m_cameraMovement.x = 0;
-						break;
-				}
-			}
-		}
+    }
 
-		@Override
-		public void mouseEvent(InputMouseEvent event) {
-			if (event.mouseButton == InputMouseEvent.MouseButton.Left && event.type == InputMouseEvent.MouseEventType.MouseClicked) {
-				IEntity tile = m_worldView.pick(IEntity.class, event.location);
-				if (tile != null) {
-					m_camera.lookAt(tile.getBody().getLocation());
-				}
-			}
-		}
+    private class CameraController implements Window.IWindowFocusObserver, Timer.ITimerObserver, WorldView.IWorldViewInputObserver {
 
-	}
+        private Vector3F m_cameraMovement = new Vector3F();
+        private float m_snapGridSize = 0.5F;
+        private final WorldView m_worldView;
+
+        public CameraController(WorldView worldView) {
+            m_worldView = worldView;
+        }
+
+        @Override
+        public void update(int deltaTime) {
+            if (!m_cameraMovement.isZero()) {
+                m_camera.move(m_cameraMovement.normalize().multiply(deltaTime / 200.0F * m_snapGridSize));
+            }
+            float x = (int) (m_camera.getLookAt().x / m_snapGridSize) * m_snapGridSize;
+            float y = Math.round(m_camera.getLookAt().y / m_snapGridSize) * m_snapGridSize;
+            m_world.getCursor().setLocation(new Vector3F(x, y, m_camera.getLookAt().z));
+        }
+
+        public float getCursorSnapGridSize() {
+            return m_snapGridSize;
+        }
+
+        public void setCursorSnapGridSize(float gridSize) {
+            m_snapGridSize = Math.max(gridSize, 0.01F);
+        }
+
+        @Override
+        public void onFocusChanged(boolean hasFocus) {
+            if (!hasFocus) {
+                m_cameraMovement = new Vector3F();
+            }
+        }
+
+        @Override
+        public void keyEvent(InputKeyEvent e) {
+            if (e.type == InputKeyEvent.KeyEventType.KeyDown) {
+                switch (e.keyCode) {
+                    case KeyEvent.VK_UP:
+                        m_cameraMovement.y = -1;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        m_cameraMovement.x = 1;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        m_cameraMovement.y = 1;
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        m_cameraMovement.x = -1;
+                        break;
+                }
+            } else if (e.type == InputKeyEvent.KeyEventType.KeyUp) {
+                switch (e.keyCode) {
+                    case KeyEvent.VK_UP:
+                    case KeyEvent.VK_DOWN:
+                        m_cameraMovement.y = 0;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                    case KeyEvent.VK_LEFT:
+                        m_cameraMovement.x = 0;
+                        break;
+                }
+            }
+        }
+
+        @Override
+        public void mouseEvent(InputMouseEvent event) {
+            if (event.mouseButton == InputMouseEvent.MouseButton.Left && event.type == InputMouseEvent.MouseEventType.MouseClicked) {
+                IEntity tile = m_worldView.pick(IEntity.class, event.location);
+                if (tile != null) {
+                    m_camera.lookAt(tile.getBody().getLocation());
+                }
+            }
+        }
+
+    }
 }
